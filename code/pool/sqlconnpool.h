67d7b72876d4c2edc7b15a29ce6bd508d862a280
JasonLaw <luozuxuan@foxmail.com>
2022-07-29 15:29:13 +0800

    Init project.

---
+++ code/pool/sqlconnpool.h
@@ -0,0 +1,47 @@
+/*
+ * @Author: JasonLaw
+ * @Date: 2022-05-08 11:00:38
+ * @LastEditors: JasonLaw
+ * @LastEditTime: 2022-05-09 14:55:21
+ * @FilePath: /WebServer-master/code/pool/sqlconnpool.h
+ * @Description: 
+ */
+#ifndef SQLCONNPOOL_H
+#define SQLCONNPOOL_H
+
+#include <mysql/mysql.h>
+#include <string>
+#include <queue>
+#include <mutex>
+#include <semaphore.h>
+#include <thread>
+#include "../log/log.h"
+
+class SqlConnPool {
+public:
+    static SqlConnPool *Instance();
+
+    MYSQL *GetConn();
+    void FreeConn(MYSQL * conn);
+    int GetFreeConnCount();
+
+    void Init(const char* host, int port,
+              const char* user,const char* pwd, 
+              const char* dbName, int connSize);
+    void ClosePool();
+
+private:
+    SqlConnPool();
+    ~SqlConnPool();
+
+    int MAX_CONN_;                  // 最大连接数
+    int useCount_;                  // 当前连接数
+    int freeCount_;                 // 空闲连接数
+
+    std::queue<MYSQL *> connQue_;   // 队列
+    std::mutex mtx_;                // 互斥锁
+    sem_t semId_;                   // 信号量
+};
+
+
+#endif // SQLCONNPOOL_H
